482ae75ed8b0229c00a547285343bf4585cf93b1
Indemberge Santos <indemberge@yahoo.com.br>
2019-06-13 19:14:00 -0300

    criada a conexao com o MongoDB no arquivo server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// iniciando o App
 const app = express();
+
+// iniciando o DB
+mongoose.connect('mongodb+srv://indemberge:859621e@cursonodejs-rtjkg.mongodb.net/test?retryWrites=true',{
+useNewUrlParser: true
+});
 
 app.get('/', (get, res) => {
     res.send("Hello Planets");
